ea3a9b9ee9c6f18a85dcb4f4003f6d63858ed101
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-11 16:53:49 -0600

    commit tree 7319152d653dbc6498fccf3df1c1be45dee64a12

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do | name, index |
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+def summon_captain_planet(elements)
+elements.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(x)
+x.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(yum)
   cheese_types = ["cheddar", "gouda", "camembert"]
+yum.find do |yum|
+  cheese_types.include?(yum)
+  end
 end
+
+
+
+
+
